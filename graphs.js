1a130665557c8c3945aa1e345c7c11db0a01d32f
Andy Short <andynshort@gmail.com>
2015-03-04 01:55:24 -0800

    create elements programmatically

--- graphs.js
+++ graphs.js
@@ -20,18 +20,24 @@
       }
     }
 
-    var svg = document.createElementNS(svgns, 'svg');
-    svg.setAttributeNS(null, 'height', config.height);
-    svg.setAttributeNS(null, 'width', config.width);
-    svg.style.display = 'block';
+    var elements = Object.create(null);
+    for (var key in frequencyMap) {
 
-    var circle = document.createElementNS(svgns, 'circle');
-    circle.setAttributeNS(null, 'cx', 50);
-    circle.setAttributeNS(null, 'cy', 50);
-    circle.setAttributeNS(null, 'r', 50);
-    circle.style.fill = 'red';
+      var svg = document.createElementNS(svgns, 'svg');
+      svg.setAttributeNS(null, 'height', config.height);
+      svg.setAttributeNS(null, 'width', config.width);
+      svg.style.display = 'block';
 
-    svg.appendChild(circle);
-    return {2: svg.cloneNode(true), 3: svg.cloneNode(true), 4: svg.cloneNode(true), 5: svg.cloneNode(true), 6: svg.cloneNode(true), 7: svg.cloneNode(true), 8: svg.cloneNode(true), 9: svg.cloneNode(true), 10: svg.cloneNode(true), 11: svg.cloneNode(true), 12: svg.cloneNode(true)};
+      var circle = document.createElementNS(svgns, 'circle');
+      circle.setAttributeNS(null, 'cx', 50);
+      circle.setAttributeNS(null, 'cy', 50);
+      circle.setAttributeNS(null, 'r', 50);
+      circle.style.fill = 'red';
+
+      svg.appendChild(circle);
+
+      elements[key] = svg;
+    }
+    return elements;
   };
 })();
